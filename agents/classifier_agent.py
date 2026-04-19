c914c05c55321b915b5368fc53a736cc234d7d96
Me-lab-c <manvithadevathi@gmail.com>
2026-04-19 10:37:57 +0530

    Delete agents directory

--- agents/classifier_agent.py
+++
@@ -1,7 +0,0 @@
-def classify_document(text):
-    if text.count("?") > 5:
-        return "Questionnaire/Form"
-    elif "invoice" in text.lower():
-        return "Invoice"
-    else:
-        return "General Document"
